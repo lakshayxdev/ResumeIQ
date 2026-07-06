2be9a3965eda78dd88a1a238ea1ca9f74558f020
Lakshay <161314423+lakshayxdev@users.noreply.github.com>
2026-07-06 14:02:04 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,5 @@
-
-
 import AppRoutes from "./routes/AppRoutes"
 import { AnalysisProvider } from "./context/AnalysisContext"
-// import { useState } from "react"
-// import SplashScreen from "./components/SplashScreen";
-
-// const App = () => {
-//   const [loading, setLoading] = useState(true);
-//   return (
-//     <>
-//     {loading? (
-//       <SplashScreen onFinish={() => setLoading(false)}/>
-//     ) : (
-    //    <AnalysisProvider>
-    // <AppRoutes />
-    // </AnalysisProvider>
-//     )
-//   }
-   
-//     </>
-//   )
-// }
-
-// export default App
-
 import { useEffect, useState } from "react";
 import Preloader from "./preloader/PreLoader";
 
